a916e9a41dad6582574ebaf74b3619acaebad9ee
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-10-06 02:42:09 -0500

    Swapped ALL 3D assets to 2D

--- Limb Scenes/Scripts/base_body_part.gd
+++ Limb Scenes/Scripts/base_body_part.gd
@@ -3,7 +3,7 @@ class_name BaseBodyPart extends Node3D
 ## Body part resource to store moveset data, health, and damage
 @export var body_part_resource : BodyPartResource
 ## Mesh for the specific body part
-@export var mesh_instance_3d: MeshInstance3D
+@export var mesh_instance_3d: Sprite3D
 
 ## Returns the local BodyPartResource
 func get_body_part_resource(): return body_part_resource
