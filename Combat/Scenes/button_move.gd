f6a7abc3d5ca384f0d6a6c335cc46086657c0d57
Athish <Athish1123@gmail.com>
2025-10-05 23:35:50 -0500

    Random Button Texture

--- Combat/Scenes/button_move.gd
+++ Combat/Scenes/button_move.gd
@@ -1,12 +1,27 @@
 class_name ButtonInfo
-extends Button
+extends TextureButton
 
 signal sendMove(sentMove : Move)
 
 @onready var label: Label = %Label
 @onready var texture_rect: TextureRect = %TextureRect
 
+
+var array = [preload("uid://bnk17wb0merph"), preload("uid://bvbmec45drrsh"), preload("uid://ccvlnjv3ovsg3"), preload("uid://did5gkagterjm"), preload("uid://1f7f67yoaxph"), preload("uid://bk0dx36dm4nu1"), preload("uid://b5p5jq5bb80ly"), preload("uid://cw3uslkubdvnb"), preload("uid://ibkyxacbjeyv"), preload("uid://btwbcsixq6sa6"), preload("uid://c5hndo5jsxk5o"), preload("uid://bhbsjsqq2jnds"), preload("uid://bebdhx0codola"), preload("uid://hteefrnsp3ev"), preload("uid://c6lrqugok3tut"), preload("uid://d2ehyein1mdf6"), preload("uid://dw7pco2gwobf7"), preload("uid://bckbj02qqikdn"), preload("uid://cm2ko6dm4t4p8"), preload("uid://uoqpa2g3lgeb")]
+
+
 var move : Move 
+var rNum : int
+
+func _ready() -> void:
+	setTextureButton(randomTexture())
+
+func randomTexture():
+	return array.pick_random()
+
+
+func setTextureButton(textureLoad) ->void:
+	texture_normal = textureLoad
 
 
 func setText() -> void:
@@ -32,3 +47,4 @@ func setupVisuals() -> void:
 
 func _on_pressed() -> void:
 	sendMove.emit(move)
+	
--- Main Menu/main_menu.gd
+++ Main Menu/main_menu.gd
@@ -3,24 +3,51 @@ extends Node2D
 @onready var sfx_volume: HSlider = $MarginContainer/VBoxContainer/SettingMenu/HBoxContainer3/SFXVolume
 @onready var music_volume: HSlider = $MarginContainer/VBoxContainer/SettingMenu/HBoxContainer2/MusicVolume
 @onready var main_volume: HSlider = $MarginContainer/VBoxContainer/SettingMenu/HBoxContainer/MainVolume
-@onready var play: Button = $MarginContainer/VBoxContainer/MainButtons/Play
-@onready var settings: Button = $MarginContainer/VBoxContainer/MainButtons/Settings
-@onready var credits: Button = $MarginContainer/VBoxContainer/MainButtons/Credits
-@onready var quit: Button = $MarginContainer/VBoxContainer/MainButtons/Quit
+@onready var play: TextureButton = $MarginContainer/VBoxContainer/MainButtons/Play
+@onready var settings: TextureButton = $MarginContainer/VBoxContainer/MainButtons/Settings
+@onready var credits: TextureButton = $MarginContainer/VBoxContainer/MainButtons/Credits
+@onready var quit: TextureButton = $MarginContainer/VBoxContainer/MainButtons/Quit
 @onready var fullscreen: CheckBox = $MarginContainer/VBoxContainer/SettingMenu/Fullscreen
 @onready var main_buttons: VBoxContainer = $MarginContainer/VBoxContainer/MainButtons
 @onready var setting_menu: VBoxContainer = $MarginContainer/VBoxContainer/SettingMenu
-@onready var back: Button = $MarginContainer/VBoxContainer/SettingMenu/Back
+@onready var back: TextureButton = $MarginContainer/VBoxContainer/SettingMenu/Back
 @onready var credits_menu: VBoxContainer = $MarginContainer/VBoxContainer/CreditsMenu
-@onready var credits_back: Button = $MarginContainer/VBoxContainer/CreditsMenu/Back
+@onready var credits_back: TextureButton = $MarginContainer/VBoxContainer/CreditsMenu/Back
 
+var button_textures = [preload("uid://bnk17wb0merph"), preload("uid://bvbmec45drrsh"), preload("uid://ccvlnjv3ovsg3"), preload("uid://did5gkagterjm"), preload("uid://1f7f67yoaxph"), preload("uid://bk0dx36dm4nu1"), preload("uid://b5p5jq5bb80ly"), preload("uid://cw3uslkubdvnb"), preload("uid://ibkyxacbjeyv"), preload("uid://btwbcsixq6sa6"), preload("uid://c5hndo5jsxk5o"), preload("uid://bhbsjsqq2jnds"), preload("uid://bebdhx0codola"), preload("uid://hteefrnsp3ev"), preload("uid://c6lrqugok3tut"), preload("uid://d2ehyein1mdf6"), preload("uid://dw7pco2gwobf7"), preload("uid://bckbj02qqikdn"), preload("uid://cm2ko6dm4t4p8"), preload("uid://uoqpa2g3lgeb")]
+@onready var button_array : Array[TextureButton] = [play, settings, credits, quit, back, credits_back]
 
 func _ready() -> void:
+	buttonTexture()
 	play.grab_focus()
 	fullscreen.button_pressed = true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else false
 	main_volume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
 	music_volume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("MUSIC")))
 	sfx_volume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
+
+#func randomTexture():
+	#return array.pick_random()
+
+
+func setTextureButton(button, textureLoad) ->void:
+	button.texture_normal = textureLoad
+
+func buttonTexture():
+	
+	for button in button_array:
+		button.texture_normal = button_textures.pick_random()
+	
+	
+	#for i in range (0,7):
+		#setTextureButton(bArray[i], randomTexture())
+	
+
+#func setTextureOnButtons():
+	#settings.texture_normal
+	#credits.texture_normal
+ 	#quit.texture_normal
+	#back.texture_button
+	#credits.texture_normal
 
 func _on_play_pressed() -> void:
 	print('change scene to tutorial scene')
